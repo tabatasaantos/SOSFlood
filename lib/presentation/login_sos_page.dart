87052575090c82026993bfccbcf752650b5ab7d5
tabata.santos <JoeyMadrug@2..>
2024-05-07 15:58:32 -0300

    update UI login page

--- lib/presentation/login_sos_page.dart
+++ lib/presentation/login_sos_page.dart
@@ -21,41 +21,33 @@ class LoginSOSPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(height: 50),
-
+              const SizedBox(height: 30),
               const Icon(
-                Icons.lock,
+                Icons.sos_outlined,
                 size: 100,
+                color: Color.fromARGB(255, 123, 41, 36),
               ),
-
-              const SizedBox(height: 50),
-
-              Text(
-                'Bem-vindo!',
+              const Text(
+                'Enchentes',
                 style: TextStyle(
-                  color: Colors.grey[700],
-                  fontSize: 16,
+                  color: Color.fromARGB(255, 123, 41, 36),
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-
-              const SizedBox(height: 25),
-
+              const SizedBox(height: 50),
               TextFieldSOSWidget(
                 controller: usernameController,
                 hintText: 'Telefone',
                 obscureText: false,
               ),
-
               const SizedBox(height: 10),
-
               TextFieldSOSWidget(
                 controller: passwordController,
                 hintText: 'Senha',
                 obscureText: true,
               ),
-
               const SizedBox(height: 10),
-
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -68,15 +60,11 @@ class LoginSOSPage extends StatelessWidget {
                   ],
                 ),
               ),
-
-              const SizedBox(height: 25),
-
+              const SizedBox(height: 30),
               ButtonSOSWidget(
                 onTap: signUserIn,
               ),
-
               const SizedBox(height: 50),
-
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 25.0),
                 child: Row(
@@ -90,7 +78,7 @@ class LoginSOSPage extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 10.0),
                       child: Text(
-                        'Ou continue com',
+                        'Ou acesse com',
                         style: TextStyle(color: Colors.grey[700]),
                       ),
                     ),
@@ -103,35 +91,31 @@ class LoginSOSPage extends StatelessWidget {
                   ],
                 ),
               ),
-
               const SizedBox(height: 50),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
                   TileSOSWidget(imagePath: 'lib/images/google.png'),
                 ],
               ),
-
-              const SizedBox(height: 50),
-
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     Text(
-              //       'Not a member?',
-              //       style: TextStyle(color: Colors.grey[700]),
-              //     ),
-              //     const SizedBox(width: 4),
-              //     const Text(
-              //       'Register now',
-              //       style: TextStyle(
-              //         color: Colors.blue,
-              //         fontWeight: FontWeight.bold,
-              //       ),
-              //     ),
-              //   ],
-              // )
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                  const SizedBox(width: 4),
+                  const Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
