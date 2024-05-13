e634464aaa097406f1943382897a34ebe3a4ff58
tabata.santos <tabata.gabriela@hotmail.com>
2024-05-13 14:12:15 -0300

    add database

---
+++ lib/database/db_location_sos.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geoflutterfire/geoflutterfire.dart';
+
+class DB {
+  DB._();
+  static final DB _instance = DB._();
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  static get() {
+    return DB._instance._firestore;
+  }
+
+static setupDemoData() async {
+    FirebaseFirestore firestore = DB.get();
+    final geo = Geoflutterfire();
+
+    
+}}
